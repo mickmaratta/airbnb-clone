a83cc04b4e5c96cde11fefeca1782d5475c760d9
Mick Maratta <mickmaratta@gmail.com>
2022-11-02 10:08:07 -0400

    Index page frontend completed

---
+++ components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100'>
+        <div className='space-y-4 text-xs text-gray-600'>
+            <h5 className='font-bold'>ABOUT</h5>
+            <p>How AirBnB works</p>
+            <p>Newsroom</p>
+            <p>Investors</p>
+            <p>AirBnB Plus</p>
+            <p>AirBnB Luxe</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>COMMUNITY</h5>
+            <p>Accessibility</p>
+            <p>This is not a real site</p>
+            <p>Its a pretty awesome clone</p>
+            <p>Refferals accepted</p>
+            <p>Carlos Cat</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>HOST</h5>
+            <p>Become a host</p>
+            <p>Another link</p>
+            <p>Something else</p>
+            <p>Useful info</p>
+            <p>You are awesome</p>
+        </div>
+        <div className='space-y-4 text-xs text-gray-800'>
+        <h5 className='font-bold'>SUPPORT</h5>
+            <p>Help Centre</p>
+            <p>Trust & Safety</p>
+            <p>Say Hi</p>
+            <p>Contact</p>
+            <p>Contact Carlos</p>
+        </div>
+        <div></div>
+    </footer>
+  )
+}
+
+export default Footer
